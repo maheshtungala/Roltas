859886ca073ed8531f0175a8529cbebfc17d77c6
QAPLANET <QAPLANET@QAPLANET-PC>
2019-02-05 15:15:48 +0530

    committed by Roltas on 5-Feb

---
+++ Roltas-Maven/src/main/java/myPack1/Rolta.java
@@ -0,0 +1,13 @@
+package myPack1;
+import org.testng.Assert;
+import org.testng.annotations.*;
+public class Rolta {
+@Test
+public static void m1() {
+	Assert.assertEquals(11, 11);
+}
+@Test
+public static void m2() {
+	Assert.assertEquals(11, 11);
+}
+}
